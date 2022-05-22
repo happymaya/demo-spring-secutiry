347f93dc0baa5c627de12f72217e774d1d8d3971
happymaya <104907109+happymaya@users.noreply.github.com>
2022-05-22 21:18:27 +0800

    delete

---
+++ sample/src/main/java/cn/happymaya/sample/controller/DemoController.java
@@ -0,0 +1,13 @@
+package cn.happymaya.sample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
